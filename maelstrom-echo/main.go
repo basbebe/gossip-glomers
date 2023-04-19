d76e0a2a8a3217c69b64f203fb04a7eed9b3c307
basbebe <git@bastianbeuttel.com>
2023-04-19 17:27:49 +0200

    Add maelstrom-echo

---
+++ maelstrom-echo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as a loosely-typed map.
+		var body map[string]any
+		err := json.Unmarshal(msg.Body, &body)
+		if err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "echo_ok"
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	err := n.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
